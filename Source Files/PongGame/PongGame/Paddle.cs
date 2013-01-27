1fbaf07bd44a455aec3818e7d1a7f1dd505da815
David Birchall <kal_agrim@hotmail.co.uk>
2013-01-27 13:39:51 -0800

    Pong Game v1.1

---
+++ Source Files/PongGame/PongGame/Paddle.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PongGame
+{
+    interface Paddle : Quad, GameObjects
+    {
+        double PositionX { get; set; }
+        int PositionY { get; set; }
+    }
+}
---
+++ Source Files/PongGame/PongGame/Quad.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PongGame
+{
+    interface Quad : GameObjects
+    {
+        int Height { get; set; }
+        int Width { get; set; }
+    }
+}
